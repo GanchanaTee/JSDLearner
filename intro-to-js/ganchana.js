bda7d162db04ff5d6dac5e3bbc713f80a404d315
ganchana youpaisan <ganchana78@gmail.com>
2022-02-18 13:30:36 +0700

    learn js

--- intro-to-js/ganchana.js
+++ intro-to-js/ganchana.js
@@ -1,11 +1,23 @@
 function askMyName() {
     const name = prompt("Please enter your name");
     if (name != null) {
-       document.getElementById("my-name").innerHTML = "My name is " + name;
+       document.getElementById("myname").innerHTML = "My name is " + name;
     }
  }
  
  function helloWorld() {
-    const h1message = document.getElementById("my-name").innerHTML
-    alert(h1message + " แอบแซง...เพราะแรงน้อย")
+    const h1message = document.getElementById("my-name").innerHTML ;
+    alert(h1message + " แอบแซง...เพราะแรงน้อย");
+ }
+
+ function calAge() {
+    let birthYear = prompt("Please enter your birth year");
+    yourAge = 2022 - Number(birthYear);
+    alert(`ยินดีด้วย นายอยู่ในประเทศนี้มา ${yourAge} ปีแล้ว`);
+ }
+ function loadProfile() {
+    myName = "Ganchana" ;
+    myProvince = "Pathumtani" ;
+    document.getElementById("my-name").innerHTML = myName ;
+    document.getElementById("my-province").innerHTML = myProvince;
  }
